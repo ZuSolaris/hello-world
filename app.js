721dbe89b916bcc1af7458451daee5c1f851714a
Zoe Gonzalez <zusolaris.personal@gmail.com>
2022-08-31 19:05:59 -0700

    added button, added interactive prompt

---
+++ app.js
@@ -0,0 +1,35 @@
+'use scrict';
+
+let inpt = prompt ('What is your favorite phenomena of the three?');
+
+
+
+
+
+
+
+
+
+
+ if (inpt === '1') {
+    message = '     Wow thats my favorite too!'
+ }
+
+ else if (inpt === '2'){
+    message = 'Awesome! Did you know those are the only man made ones on this list.'
+ }
+
+ else if (inpt === '3') {message = 'Typical... I mean yay.... everyone loves this!'
+
+ }
+else {
+(message) = 'That is not a valid answer!'}
+
+document.write ('Great Choice!  ');
+
+
+document.write(message);
+
+
+
+
