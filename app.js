d119ec74fb9d725596b66760f097f0c48951e2e8
Zoe Gonzalez <zusolaris.personal@gmail.com>
2022-09-01 18:34:48 -0700

    added universe questionnaire

--- app.js
+++ app.js
@@ -9,21 +9,46 @@
 let message = ''
 let ok
 
+
+function hme(){
+   let hmd = prompt('Welcome to design your own universe! How many planets do you want in here? (1-10)');
+for (let i = 0; i < hmd && i < 10; i++){
+   document.write('<img src="Earth.gif"/>');
+}
+
+}
+function questhme() {
+   let inpt = prompt ('Would you like to design a universe?');
+   
+    if (inpt === 'yes'){hme();}
+
+    else if (inpt === 'no'){
+       message =  'Enjoy your planetless universe!'
+    }
+  
+   else{
+   (message) = 'That is not a valid answer!'}
+   }
+
+
+
 function customMessage(){
 let inpt = prompt ('What is your favorite phenomena of the three?');
 
- if (inpt === '1') { message ='     Wow thats my favorite too!'
+ if (inpt === '1') { message ='     Wow thats my favorite too!'; 
+ questhme();
  }
  else if (inpt === '2'){
-    message =  'Awesome! Did you know those are the only man made ones on this list.'
+    message =  'Awesome! Did you know those are the only man made ones on this list.';questhme();
  }
- else if (inpt === '3') {message = 'Typical... I mean yay.... everyone loves this!'
+ else if (inpt === '3') {message = 'Typical... I mean yay.... everyone loves this!';questhme();
 
  } 
 
 else{
 (message) = 'That is not a valid answer!'}
 }
+
 
 
 function ask(){
@@ -34,8 +59,7 @@ function ask(){
 document.write ('I understand!  ');
 
 if (ques == 'no') {message ='Please read the page and refresh!'};
-
-if (ques == '') {message = 'Thats not a valid answer!'};
+if (ques == '') {message = ' or not..... Thats not a valid answer!'};
 
 
 document.write(message);
@@ -44,16 +68,10 @@ document.write(message);
 
 document.write(message);
 ask()
+
 // //parameters
 // function addTwoNumbers(num1, num2)
 // {return num1 + num2; }
 // //                              arguments
 // let myNewNumber = addTwoNumbers (10,10)
 // console.log(myNewNumber);
-
-
-
-
-
-
-
---
+++ readme.md
@@ -0,0 +1,17 @@
+
+A for-loop repeats until a specified conditions evaluates to false
+
+A for-loop is generally used when we know the number of times we need it ahead of time
+
+for(intial value ; condition to value)
+for (intial clause; condition to evaluate; increment) code to execute
+A while-loop repeats until a specified condition evaluated to false
+Used when we dont know how many times it going to take.
+
+Structure of  while loop
+
+while(condition is true){execute this code}
+
+let x = true 
+
+while x = ()
